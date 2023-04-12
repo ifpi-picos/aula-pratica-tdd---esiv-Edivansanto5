54f37648636549f64eff8342ae25651788f100d6
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-04-12 19:45:39 -0300

    primeiro teste de calcular o dolar

--- Dollar.java
+++ Dollar.java
@@ -3,10 +3,10 @@ class Dollar {
     Dollar(int amount) {}
     void times(int multiplier) {}
 
-    public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-    }
 }
 
+public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertEquals(10, five.amount);
+}

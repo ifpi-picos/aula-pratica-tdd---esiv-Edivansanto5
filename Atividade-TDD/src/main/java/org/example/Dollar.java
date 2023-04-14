0b6caa060e8828d3e1d903be70ed670715d4314a
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-04-14 16:43:12 -0300

    Dando uma implementação mais real para a classe Dollar

--- Atividade-TDD/src/main/java/org/example/Dollar.java
+++ Atividade-TDD/src/main/java/org/example/Dollar.java
@@ -1,7 +1,20 @@
 package org.example;
 
 public class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}
-    void times(int multiplier) {}
+    // criando uma variável
+    int amount ;
+
+    // construtod
+    Dollar(int amount) {
+        this.amount = amount;
+        // Isso atribui o valor do parâmetro amount à variável amount do objeto Dollar
+        // atualmente em construção.
+    }
+    void times (int multiplier){
+        // definindo um metado TIMES que multiplica a quantia em dólar por um valor multiplier..
+
+        amount = amount * multiplier;
+        // Isso multiplica o valor atual da variável amount pelo valor do parâmetro
+        // multiplier e atribui o resultado de volta à variável amount.
+    }
 }

0af382a3515fe2532076ab1a1149562300ea44b9
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-04-15 16:32:09 -0300

    voltando para o codigo padrao

--- demo/src/main/java/com/example/Dollar.java
+++ demo/src/main/java/com/example/Dollar.java
@@ -6,7 +6,7 @@ public class Dollar {
         this.amount = amount;
     }			
     Dollar times(int multiplier) {
-        amount = amount * amount *multiplier ;
+        amount = amount * multiplier ;
         return null;
     }  
 }

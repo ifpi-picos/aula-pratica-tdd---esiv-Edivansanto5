66a5b76f74a7d57f605f25ce243f1557921ba833
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-04-17 12:39:46 -0300

    Implementando equals (método da classe Dollar):

--- tdd/src/main/java/org/example/Dollar.java
+++ tdd/src/main/java/org/example/Dollar.java
@@ -9,13 +9,17 @@ public class Dollar {
          public Dollar times(int multiplier) {
                 return new Dollar(amount * multiplier);
         }
-        public boolean equals(Object object) {
-                if (object instanceof Dollar) {
-                        Dollar dollar = (Dollar) object;
-                        return amount == dollar.amount;
-                } else {
-                        return false;
-                }
+        public boolean equals(Object object)  {
+                Dollar dollar = (Dollar) object;
+                return amount == dollar.amount;
         }
+//        public boolean equals(Object object) {
+//                if (object instanceof Dollar) {
+//                        Dollar dollar = (Dollar) object;
+//                        return amount == dollar.amount;
+//                } else {
+//                        return false;
+//                }
+//        }
 
 }

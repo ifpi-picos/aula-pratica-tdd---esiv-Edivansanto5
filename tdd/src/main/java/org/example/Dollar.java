7797fee0384c846d3f02619b4677fd3cb32402b4
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-04-17 09:26:10 -0300

    vendo pasta

---
+++ tdd/src/main/java/org/example/Dollar.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Dollar {
+        public int amount = 10;
+        public Dollar(int amount){
+
+        }
+        public void times(int multiplier){
+/*fdfs*/
+
+        }
+}

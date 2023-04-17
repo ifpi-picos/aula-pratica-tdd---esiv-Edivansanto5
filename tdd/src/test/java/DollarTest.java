2723670d802f891d0a767d89a4e87218a2e2f0c7
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-04-17 12:37:15 -0300

    alterando a variavel para private & modificando a class de teste

--- tdd/src/test/java/DollarTest.java
+++ tdd/src/test/java/DollarTest.java
@@ -1,18 +1,11 @@
 import org.example.Dollar;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-
 public class DollarTest {
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
-
-
-
     }
 }

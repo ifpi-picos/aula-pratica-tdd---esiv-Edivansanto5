999601164b01c84a144f54b47a4db24246d8a344
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-04-19 20:28:31 -0300

    finalizando primeiro teste

--- tdd/src/main/java/org/example/Dollar.java
+++ tdd/src/main/java/org/example/Dollar.java
@@ -1,16 +1,9 @@
 package org.example;
 
 public class Dollar {
-        private int amount ;
-        public Dollar(int amount){
-                this.amount = amount;
+        public int amount = 10;
 
-        }
-         public Dollar times(int multiplier) {
-                return new Dollar(amount * multiplier);
-        }
-        public boolean equals(Object object)  {
-                Dollar dollar = (Dollar) object;
-                return amount == dollar.amount;
-        }
+        public Dollar(int amount) {}
+        public void times(int multiplier) {}
+
 }
--- tdd/src/test/java/DollarTest.java
+++ tdd/src/test/java/DollarTest.java
@@ -3,22 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DollarTest {
     @Test
-    public void testMultiplication(){
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+    public void testMultiplication() {
+
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
     }
-    class Franc{
-        private int amount;
-        Franc (int amount){
-            this.amount= amount;
-        }
-       public  Franc times(int multiplier){
-           return new Franc(amount * multiplier);
-       }
-        public boolean equals(Object object) {
-            Franc franc = (Franc) object;
-            return amount == franc.amount;
-        }
-    }
+
 }

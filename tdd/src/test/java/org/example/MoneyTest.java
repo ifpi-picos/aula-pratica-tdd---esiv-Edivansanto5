c910f06329b0fcdf4b10629b63fad51fdfcc5c75
Edivansanto5 <edivansantos1478@gmail.com>
2023-04-24 10:09:25 -0300

    criando e testando a classe de Money e MoneyTest

--- tdd/src/test/java/org/example/MoneyTest.java
+++ tdd/src/test/java/org/example/MoneyTest.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class MoneyTest {
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+     }
 }

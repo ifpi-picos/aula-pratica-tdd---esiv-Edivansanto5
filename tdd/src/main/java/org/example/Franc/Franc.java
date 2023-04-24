fdbef294b88f026c0b9335b969c615416c64ea84
Edivansanto5 <edivansantos1478@gmail.com>
2023-04-24 12:33:30 -0300

    teste com sucesso da franc

--- tdd/src/main/java/org/example/Franc/Franc.java
+++ tdd/src/main/java/org/example/Franc/Franc.java
@@ -1,11 +1,11 @@
 package org.example.Franc;
 
-class Franc {
+public class Franc {
     private int amount;
-    Franc(int amount) {
+    public Franc(int amount) {
         this.amount= amount;
     }
-    Franc times(int multiplier)  {
+    public Franc times(int multiplier)  {
         return new Franc(amount * multiplier);
     }
     public boolean equals(Object object) {
